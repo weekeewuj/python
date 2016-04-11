939a24d2b8221002a712ff4129cc9f4a557284d1
weekeewu <Weekeew@users.noreply.github.com>
2016-04-11 22:52:25 +0800

    Added files via upload

---
+++ 160411.py
@@ -0,0 +1,10 @@
+import gzip
+def ungzip(data):
+    try:        
+# 尝试解压
+        print('正在解压.....')
+        data = gzip.decompress(data)
+        print('解压完毕!')
+    except:
+        print('未经压缩, 无需解压')
+    return data
