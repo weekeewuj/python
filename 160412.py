a660489d568b90429a09531d3faf4d8082ccb754
weekeewu <Weekeew@users.noreply.github.com>
2016-04-12 23:25:10 +0800

    Added files via upload

---
+++ 160412.py
@@ -0,0 +1,16 @@
+number = 5
+ 
+def myfunc():
+    
+    print number
+ 
+def anotherfunc():
+    
+    
+    print number
+    number = 3
+ 
+def yetanotherfunc():
+    global number
+    
+    number = 3
